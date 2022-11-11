c788e52178f56a36893d016ea4af84981593480e
Philip <krenity@gmail.com>
2022-11-11 22:29:48 +0000

    UPDATES

--- player.gd
+++ player.gd
@@ -1,16 +1,35 @@
 extends KinematicBody2D
 
-var velocity = Vector2.ZERO
-
+var LowJump = preload("res://Assets/Images/LowJump.png")
+var MediumJump = preload("res://Assets/Images/MediumJump.png")
+var HighJump = preload("res://Assets/Images/HighJump.png")
+const UP = Vector2(0, -1)
+const speed = 230
+const gravity = 10
+const maxgravity = 720
+var direction = Vector2()
+func _ready():
+	pass
 func _physics_process(delta):
-	velocity.y += 4
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 50
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -50
+	direction.y += gravity
+	if direction.y > maxgravity:
+		direction.y = maxgravity
+	print(direction)
+	if Input.is_action_pressed("move_right") and Input.is_action_pressed("move_down"):
+		if is_on_floor():
+			direction.x = speed
+			direction.y += -500
+	elif Input.is_action_pressed("move_left") and Input.is_action_pressed("move_down"):
+		if is_on_floor():
+			direction.x = -speed
+			direction.y += -500
+		
 	else:
-		velocity.x = 0
-		
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.x = -100
-		
+		if is_on_floor():
+			direction.y = 0
+		if is_on_ceiling():
+			direction.y = 5
+		if is_on_floor():
+			direction.x = 0
+	move_and_slide(direction, UP)
+	
