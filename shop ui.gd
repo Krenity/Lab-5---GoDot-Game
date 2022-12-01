2ad1a00fc82e9269247f91304e67d54afd7dc52e
Philip <krenity@gmail.com>
2022-12-01 12:45:52 +0000

    MAp

--- shop ui.gd
+++ shop ui.gd
@@ -3,12 +3,15 @@ extends Area2D
 func _ready():
 	get_node("Panel").hide()
 	get_node("Panel2").hide()
+	get_node("Panel3").hide()
 
 func _on_Shop_Detection_ShopNeedsopening():
 	get_node("Panel").show()
 	get_node("Panel2").show()
+	get_node("Panel3").show()
 
 
 func _on_Shop_Detection_ShopNeedsClosing():
 	get_node("Panel").hide()
 	get_node("Panel2").hide()
+	get_node("Panel3").hide()
